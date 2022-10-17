306db9be452f3275f4c832f1ae54e5329739a82e
hailatGH <hailat.alx@gmail.com>
2022-10-17 15:35:45 +0300

    dev deploy and cloudsettings added

--- src/deploy_dev.sh
+++ src/deploy_dev.sh
@@ -5,15 +5,15 @@ read COMMIT
 
 cd core
 mv settings.py basesettings.py
-mv cloudsettings.py settings.py
+mv cloudsettings_dev.py settings.py
 
 cd ../../
 git add .
 git commit -m "$COMMIT"
-git push --set-upstream origin main
+git push --set-upstream origin version_1.1
 
 cd src/core/
-mv settings.py cloudsettings.py
+mv settings.py cloudsettings_dev.py
 mv basesettings.py settings.py
 cd ..
 clear
