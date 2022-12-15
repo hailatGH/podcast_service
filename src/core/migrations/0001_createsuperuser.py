eec56ca2d4463933600c029a772d69788c18cf59
hailatGH <hailat.alx@gmail.com>
2022-12-15 11:17:48 +0300

    azure initial commit

--- src/core/migrations/0001_createsuperuser.py
+++ src/core/migrations/0001_createsuperuser.py
@@ -6,7 +6,7 @@ from django.db import migrations
 
 def createsuperuser(apps, schema_editor):
     music_admin_password = "playground"
-    User.objects.create_superuser("admin", password=music_admin_password)
+    User.objects.create_superuser("admin", password="playground")
 
 
 class Migration(migrations.Migration):
