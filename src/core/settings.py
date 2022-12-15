22f1a34193a2b6f9a3e2d9e87e466220f5d438dc
hailatGH <hailat.alx@gmail.com>
2022-12-15 11:33:01 +0300

    CSRF fix

--- src/core/settings.py
+++ src/core/settings.py
@@ -5,9 +5,9 @@ from .basesettings import *
 django.utils.encoding.force_text = force_str
 
 ALLOWED_HOSTS = [
-    "podcast-service.calmgrass-743c6f7f.francecentral.azurecontainerapps.io"]
+    "https://podcast-service.calmgrass-743c6f7f.francecentral.azurecontainerapps.io"]
 CSRF_TRUSTED_ORIGINS = [
-    'podcast-service.calmgrass-743c6f7f.francecentral.azurecontainerapps.io']
+    "https://podcast-service.calmgrass-743c6f7f.francecentral.azurecontainerapps.io"]
 
 DATABASES = {
     'default': {
